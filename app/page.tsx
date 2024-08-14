ec0eb694691aaa7b4b97ade1b6a167a895b93f3a
Rahmonjon <qosimovrahmonjon0927@gmail.com>
2024-08-14 09:48:33 +0500

    bag fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { Content, Hero, Sidebar } from '@/components';
-import { BlogsType } from '@/interfaces/blog.interface';
-import { BlogService } from '@/services/blog.service';
 import { Box } from '@mui/material';
 import * as React from 'react';
 
 
 
-export default async function  Home() {
-  const blogs = await BlogService.getAllBlogs();
-    console.log(blogs);
+export default  function  Home() {
     
   return (
    <Box sx={{minHeight: '90vh'}}>
